93db1db3393518a08d1209ddf0c697cb70e9cbef
Sandeep S <sandeepsajja@gmail.com>
2026-07-20 00:24:18 +0530

    Day 10 - Completed Java Variables and Data Types

---
+++ src/day02/JavaComments.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class JavaComments {
+
+	public static void main(String[] args) {
+
+		 // This program prints a welcome message
+
+        System.out.println("Welcome to Project 50L");
+
+	}
+
+}
---
+++ src/day02/VariablesDemo.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class VariablesDemo {
+	
+	 public static void main(String[] args) {
+	int bugCount = 25;
+
+    System.out.println(bugCount);
+    
+	 }
+
+}
