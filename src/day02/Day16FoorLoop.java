ae2aaa03f5de0bd5e5da2203274a69e046a1ced7
Sandeep S <sandeepsajja@gmail.com>
2026-07-26 21:48:11 +0530

    Day 16 - Learned for loop

---
+++ src/day02/Day16FoorLoop.java
@@ -0,0 +1,15 @@
+package day02;
+
+public class Day16FoorLoop {
+
+	public static void main(String[] args) {
+		
+		for (int i = 10; i>=1;i--){
+			
+			System.out.println("Iteration : " + i);
+		}
+		
+		System.out.println("Loop Finished");
+	}
+
+}
