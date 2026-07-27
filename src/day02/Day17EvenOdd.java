9eb297a65aaf33bcd95672e7846c9d780d9d63c8
Sandeep S <sandeepsajja@gmail.com>
2026-07-27 18:04:09 +0530

    Day 17 - skip numbers, accumulator

---
+++ src/day02/Day17EvenOdd.java
@@ -0,0 +1,56 @@
+package day02;
+
+public class Day17EvenOdd {
+
+public static void main (String[] args)
+{
+	int sum = 0;
+	for (int i = 1; i<=100;i++){
+		sum = sum + i;
+	}
+	System.out.println("Sum is " + sum);
+}
+
+
+}
+
+
+//	public static void main(String[] args)
+//	{
+//		//Print even numbers from 1 to 20 using increment method
+//		
+//		for (int i = 2; i<=20; i = i+2)
+//		{
+//			System.out.println(i);
+//		}
+//	}
+//
+//}
+
+//public class Day17EvenOdd {
+//	public static void main(String[] args)
+//	{
+//		//Print even numbers from 1 to 19 using increment method
+//		
+//		for (int i = 1; i<=19; i = i+2)
+//		{
+//			System.out.println(i);
+//		}
+//	}
+//
+//}
+
+//public class Day17EvenOdd {
+//	public static void main(String[] args)
+//	{
+//		//Print even numbers from 2 to 20 using Modulus method
+//		
+//		for (int i = 2; i<=20; i++)
+//		{
+//			if(i%2 == 0){
+//			System.out.println(i);	
+//			}
+//		}
+//	}
+//
+//}
