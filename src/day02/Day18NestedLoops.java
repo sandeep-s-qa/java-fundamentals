7a53ebcee859e0d2ad782920a4f237f14df2e007
Sandeep S <sandeepsajja@gmail.com>
2026-07-28 22:43:31 +0530

    Day 18 - Nested loops and pattern programs

---
+++ src/day02/Day18NestedLoops.java
@@ -0,0 +1,38 @@
+package day02;
+
+public class Day18NestedLoops {
+
+	public static void main(String[] args) {
+
+//		for (int i = 1 ; i <=4; i++) {
+//			for (int j = 1 ; j <=4; j++) {
+//				System.out.print("* ");
+//			}
+//		System.out.println();
+//		}
+		
+		
+//		for (int i = 1 ; i <=4; i++) {
+//			for (int j = 1 ; j <=i; j++) {
+//				System.out.print("* ");
+//		}
+//			System.out.println();
+//	}
+		
+//		
+//		for (int i = 1 ; i <=4; i++) {
+//			for (int j = 1 ; j <=i; j++) {
+//				System.out.print(j + " ");
+//		}
+//			System.out.println();
+//	}
+		
+		for (int i = 1 ; i <=4; i++) {
+			for (int j = 1 ; j <=i; j++) {
+				System.out.print(i + " ");
+		}
+			System.out.println();
+	}
+
+}
+}
